24bc539791d37361564a9659cf3c07fa6bddd746
Harry Sun <zoomhgtk@gmail.com>
2016-08-12 16:55:22 +0800

    telnetlib first try

---
+++ test_telnet.py
@@ -0,0 +1,25 @@
+import telnetlib
+import time
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+def main():
+	ip_addr = '128.223.51.103'
+	username = 'rviews'
+	password = ''
+	
+	remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+	output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+	remote_conn.write(username + '\n')
+	time.sleep(2)
+	output = remote_conn.read_very_eager()
+	print output
+
+	remote_conn.write("show version"+ '\n')
+	time.sleep(4)
+	output = remote_conn.read_very_eager()
+	print output
+
+if __name__ == "__main__":
+	main()
+
