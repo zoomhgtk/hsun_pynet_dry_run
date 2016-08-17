3e1d66c391289d3b9e86e4d1aa2592aab032d9a0
Harry Sun <zoomhgtk@gmail.com>
2016-08-17 08:44:00 -0700

    not finished

---
+++ class3/get_oid_v3_for_loop.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+snmp_oids = (
+        ('sysName', '1.3.6.1.2.1.1.5.0', None),
+        ('sysUptime', '1.3.6.1.2.1.1.3.0', None),
+        ('ifDescr_fa4', '1.3.6.1.2.1.2.2.1.2.5', None),
+        ('ifInOctets_fa4', '1.3.6.1.2.1.2.2.1.10.5', True),
+        ('ifInUcastPkts_fa4', '1.3.6.1.2.1.2.2.1.11.5', True),
+        ('ifOutOctets_fa4', '1.3.6.1.2.1.2.2.1.16.5', True),
+        ()
